0a947a3fe042424d2fb2577870e4c756dfc9844e
matjack9 <matjack9@gmail.com>
2020-05-22 21:07:51 -0400

    feat: add copyright

---
+++ src/common/components/copyright/copyright.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Link as RouterLink } from '@reach/router';
+import Typography, { TypographyProps } from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { Route } from 'common/utils/constants/routes';
+
+export const Copyright: React.FC<TypographyProps> = props => (
+  <Typography variant="body2" color="textSecondary" align="center" {...props}>
+    {'Copyright © '}
+    <Link component={RouterLink} to={Route.ROOT} color="inherit">
+      Brawnzaa
+    </Link>
+    {` ${new Date().getFullYear()}`}
+  </Typography>
+);
