61a0d50693f8ef22eb69f9d1a4edc58c9f9de9aa
matjack9 <matjack9@gmail.com>
2020-05-23 00:31:44 -0400

    test: add easter egg specs

---
+++ src/common/components/EasterEgg/__tests__/EasterEgg.test.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { EasterEgg } from '../EasterEgg';
+import { KONAMI_CODES } from 'common/utils/constants/misc';
+
+describe('<EasterEgg />', () => {
+  beforeEach(() => {
+    render(<EasterEgg />);
+    const maybeDialog = screen.queryByTestId('easter-egg-dialog');
+    expect(maybeDialog).not.toBeInTheDocument();
+    KONAMI_CODES.forEach(key => {
+      fireEvent.keyUp(document, { key });
+    });
+  });
+
+  it('renders after entering the konami code', () => {
+    const dialog = screen.getByTestId('easter-egg-dialog');
+    expect(dialog).toBeInTheDocument();
+  });
+
+  it('closes after clicking the close button', async () => {
+    const dialogCloseButton = screen.getByTestId('easter-egg-dialog-close');
+    fireEvent.click(dialogCloseButton);
+    await waitForElementToBeRemoved(() => screen.queryByTestId('easter-egg-dialog'));
+    const maybeDialog = screen.queryByTestId('easter-egg-dialog');
+    expect(maybeDialog).not.toBeInTheDocument();
+  });
+
+  it('can render after closing and re-entering the konami code', async () => {
+    const dialogCloseButton = screen.getByTestId('easter-egg-dialog-close');
+    fireEvent.click(dialogCloseButton);
+    await waitForElementToBeRemoved(() => screen.queryByTestId('easter-egg-dialog'));
+    const maybeDialog = screen.queryByTestId('easter-egg-dialog');
+    expect(maybeDialog).not.toBeInTheDocument();
+    KONAMI_CODES.forEach(key => {
+      fireEvent.keyUp(document, { key });
+    });
+    const dialog = screen.getByTestId('easter-egg-dialog');
+    expect(dialog).toBeInTheDocument();
+  });
+});
