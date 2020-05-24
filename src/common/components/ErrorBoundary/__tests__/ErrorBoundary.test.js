6c058402ccd708fabf4d0562f6cc38d8076154fb
matjack9 <matjack9@gmail.com>
2020-05-24 13:53:11 -0400

    test: add error boundary spec

---
+++ src/common/components/ErrorBoundary/__tests__/ErrorBoundary.test.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ErrorBoundary } from '../ErrorBoundary';
+
+const ErrorView = () => {
+  throw 'bigly error';
+};
+
+/* ignore error in console */
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+describe('<ErrorBoundary />', () => {
+  it('catches uncaught errors and renders a fallback UI', () => {
+    render(<ErrorView />, { wrapper: ErrorBoundary });
+    const crashView = screen.getByTestId('crash-view');
+    expect(crashView).toBeInTheDocument();
+  });
+});
