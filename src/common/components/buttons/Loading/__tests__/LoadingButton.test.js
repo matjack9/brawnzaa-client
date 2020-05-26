4e35ff24ee6b629faa305bd3cde6de19d824be88
matjack9 <matjack9@gmail.com>
2020-05-25 21:57:16 -0400

    test: assert icons hide when loading

--- src/common/components/buttons/Loading/__tests__/LoadingButton.test.js
+++ src/common/components/buttons/Loading/__tests__/LoadingButton.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { LoadingButton } from '../LoadingButton';
 
 describe('<LoadingButton />', () => {
@@ -21,6 +21,27 @@ describe('<LoadingButton />', () => {
     expect(button).toBeDisabled();
     expect(button).toHaveTextContent(label);
     expect(screen.getByText(label)).not.toBeVisible();
+    expect(button).toContainElement(progressCircle);
+  });
+
+  it('hides any start and end icons when loading', () => {
+    const label = 'Hide me!';
+    const startIconTestId = 'start-icon';
+    const endIconTestId = 'end-icon';
+    const startIcon = <svg data-testid={startIconTestId}></svg>;
+    const endIcon = <svg data-testid={endIconTestId}></svg>;
+    render(
+      <LoadingButton isLoading startIcon={startIcon} endIcon={endIcon}>
+        {label}
+      </LoadingButton>
+    );
+    const button = screen.getByTestId('loading-btn');
+    const progressCircle = screen.getByTestId('loading-btn-progress-circle');
+    expect(button).toBeDisabled();
+    expect(button).toHaveTextContent(label);
+    expect(screen.getByText(label)).not.toBeVisible();
+    expect(screen.getByTestId(startIconTestId)).not.toBeVisible();
+    expect(screen.getByTestId(endIconTestId)).not.toBeVisible();
     expect(button).toContainElement(progressCircle);
   });
 });
