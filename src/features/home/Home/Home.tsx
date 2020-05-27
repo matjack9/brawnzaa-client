2f069c3f4e588ddbdf9d9cb9ade412e6f7a56cd3
matjack9 <matjack9@gmail.com>
2020-05-26 20:20:59 -0400

    feat: add countdown

--- src/features/home/Home/Home.tsx
+++ src/features/home/Home/Home.tsx
@@ -2,13 +2,50 @@ import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { RouteComponentProps } from '@reach/router';
 
+const DATE = new Date('June 20, 2020 12:00:00').getTime();
+
+const Countdown: React.FC<{ date: number }> = props => {
+  const { date } = props;
+  const [now, setNow] = React.useState(Date.now());
+
+  const diff = date - now;
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(Date.now());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  return (
+    <Typography
+      variant="h2"
+      component="p"
+      align="center"
+      color="primary"
+      gutterBottom
+      style={{ margin: '1em' }}
+    >
+      {`${days}d ${hours}h ${minutes}m ${seconds}s`}
+    </Typography>
+  );
+};
+
 export const Home: React.FC<RouteComponentProps> = () => (
   <>
     <Typography variant="h1" align="center" gutterBottom>
       B R A W N Z A A
     </Typography>
-    <Typography variant="h2" align="center" gutterBottom>
+    <Typography variant="h2" component="p" align="center" gutterBottom>
       a feat of strength
     </Typography>
+    <Countdown date={DATE} />
   </>
 );
