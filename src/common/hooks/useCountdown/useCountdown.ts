2870711f5543d1b558cea5d0d36e15453629ff90
matjack9 <matjack9@gmail.com>
2020-05-27 21:27:38 -0400

    feat: optimize and update for no time remaining

--- src/common/hooks/useCountdown/useCountdown.ts
+++ src/common/hooks/useCountdown/useCountdown.ts
@@ -3,14 +3,22 @@ import * as React from 'react';
 export const useCountdown = (datetime: number, interval: number = 1000) => {
   const [now, setNow] = React.useState(Date.now());
 
+  const timeLeft = datetime - now;
+  const isTimeLeft = timeLeft > 0;
+
   React.useEffect(() => {
     const tick = setInterval(() => {
       setNow(Date.now());
     }, interval);
+
+    if (!isTimeLeft) {
+      clearInterval(tick);
+    }
+
     return () => {
       clearInterval(tick);
     };
-  }, [interval]);
+  }, [isTimeLeft, interval]);
 
-  return datetime - now;
+  return isTimeLeft ? timeLeft : 0;
 };
