49d232e6cfd22db5a9d94ab39864e23364387e64
matjack9 <matjack9@gmail.com>
2020-06-07 14:17:26 -0400

    feat: add konami code link

--- src/common/hooks/useConsoleArt/useConsoleArt.ts
+++ src/common/hooks/useConsoleArt/useConsoleArt.ts
@@ -15,6 +15,9 @@ const ASCII_ART =
 
 const KONAMI_CODE_CTA = 'Show me the Konami Code.';
 
+const KONAMI_CODE_LINK =
+  'https://www.howtogeek.com/659611/what-is-the-konami-code-and-how-do-you-use-it/#:~:text=Up%2C%20Up%2C%20Down%2C%20Down,cheats%20that%20help%20you%20win.';
+
 export const useConsoleArt = () => {
   React.useEffect(() => {
     if (__PROD__) {
@@ -22,10 +25,10 @@ export const useConsoleArt = () => {
       console.clear();
 
       /* eslint-disable-next-line no-console */
-      console.log(`%c ${WELCOME_TEXT}`, 'background: black; color: grey');
+      console.log(`%c ${WELCOME_TEXT}`, 'background: black; color: grey;');
 
       /* eslint-disable-next-line no-console */
-      console.log(`%c ${ASCII_ART}`, 'font-weight: bold; color: red');
+      console.log(`%c ${ASCII_ART}`, 'font-weight: bold; color: red;');
 
       /* eslint-disable-next-line no-console */
       console.log('\n\n\n');
@@ -33,8 +36,17 @@ export const useConsoleArt = () => {
       /* eslint-disable-next-line no-console */
       console.log(
         `%c ${KONAMI_CODE_CTA}`,
-        'background: #222; color: white; font-size: x-large'
+        'background: #222; color: white; font-size: x-large;'
       );
+
+      /* eslint-disable-next-line no-console */
+      console.log(
+        `%c * for the ignorant: ${KONAMI_CODE_LINK}`,
+        'color: grey; font-size: x-small;'
+      );
+
+      /* eslint-disable-next-line no-console */
+      console.log('\n');
     }
     /* eslint-disable-next-line no-console */
     return console.clear;
