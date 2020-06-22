bed370413ce690865b70a50cae5faa6cda86ba67
matjack9 <matjack9@gmail.com>
2020-06-21 22:19:56 -0400

    feat: add notable datetimes

--- src/common/constants/dates.ts
+++ src/common/constants/dates.ts
@@ -1,1 +1,13 @@
-export const NEXT_BRAWNZAA_DATETIME = new Date('June 20, 2020 00:00:00').getTime();
+export const BRAWNZAA_GENESIS_DATETIME = new Date(
+  'June 21, 2014 00:00:00'
+).getTime();
+
+export const BRAWNZAA_1_DATETIME = new Date('June 20, 2015 00:00:00').getTime();
+export const BRAWNZAA_2_DATETIME = new Date('June 18, 2016 00:00:00').getTime();
+export const BRAWNZAA_3_DATETIME = new Date('June 24, 2017 00:00:00').getTime();
+export const BRAWNZAA_4_DATETIME = new Date('June 30, 2018 00:00:00').getTime();
+export const BRAWNZAA_5_DATETIME = new Date('June 22, 2019 00:00:00').getTime();
+export const BRAWNZAA_6_DATETIME = new Date('June 20, 2020 00:00:00').getTime();
+export const BRAWNZAA_7_DATETIME = new Date('June 19, 2021 00:00:00').getTime();
+
+export const NEXT_BRAWNZAA_DATETIME = BRAWNZAA_6_DATETIME;
